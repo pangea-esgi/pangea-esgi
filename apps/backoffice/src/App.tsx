b0ac2395f42cd1395e81ba5bc4a7c419c32d554c
Maelic Pepin <pmaelic@outlook.fr>
2026-02-06 12:08:24 +0100

    feat(design_system): initialize design system package with TypeScript support
    - Created package.json for @pangea-esgi/design_system with necessary configurations and dependencies.
    - Added a simple hello function in src/index.ts.
    - Configured tsup for building the package with support for ESM and CJS formats, including type definitions and source maps.

---
+++ apps/backoffice/src/App.tsx
@@ -0,0 +1,10 @@
+import { hello } from '@pangea-esgi/design_system';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Backoffice</h1>
+      <p>{hello()}</p>
+    </div>
+  )
+}
---
+++ packages/design_system/dist/index.mjs
@@ -0,0 +1,8 @@
+// src/index.ts
+var hello = () => {
+  return "Hello world";
+};
+export {
+  hello
+};
+//# sourceMappingURL=index.mjs.map
