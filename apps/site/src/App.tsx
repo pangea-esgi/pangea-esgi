d08f82459b9c998e9cf5a61596556298c8cb536b
lorenaywd <lorenamaria@outlook.fr>
2026-03-03 17:37:33 +0100

    Resolve merge conflict in App.tsx

--- apps/site/src/App.tsx
+++ apps/site/src/App.tsx
@@ -2,6 +2,8 @@ import { Header, Footer } from "@pangea-esgi/design_system/src";
 import { Route, Routes } from "react-router-dom";
 import MenuPage from "./pages/Menu";
 import ContactPage from "./pages/Contact";
+import FormInput from '@pangea-esgi/design_system/src/_components/molecules/formInput';
+import UploadInput from '@pangea-esgi/design_system/src/_components/molecules/uploadInput';
 
 export default function App() {
   return (
