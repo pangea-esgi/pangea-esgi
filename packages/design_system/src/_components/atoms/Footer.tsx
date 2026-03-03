a7d8a9dbc2be0f6791ac70c9b03d29e0d7d659d9
Sioood <theodupontpro@gmail.com>
2026-03-03 17:19:43 +0100

    fix: :bug: footer

--- packages/design_system/src/_components/atoms/Footer.tsx
+++ packages/design_system/src/_components/atoms/Footer.tsx
@@ -2,10 +2,10 @@ import "../../styles/Footer.css";
 
 export const Footer = () => {
   return (
-    <div className="pangea-footer-container">
+    <footer className="pangea-footer-container">
       <span className="pangea-footer-circle pangea-footer-circle-1" />
       <span className="pangea-footer-circle pangea-footer-circle-2" />
       <span className="pangea-footer-circle pangea-footer-circle-3" />
-    </div>
+    </footer>
   );
 };
