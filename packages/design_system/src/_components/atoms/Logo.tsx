9d6ddc61d0473449bd1d6e0a8502cdb8d61a1b29
Sioood <theodupontpro@gmail.com>
2026-03-03 18:16:56 +0100

    fix: :bug: fix logo and add type

--- packages/design_system/src/_components/atoms/Logo.tsx
+++ packages/design_system/src/_components/atoms/Logo.tsx
@@ -1,9 +1,17 @@
 import "../../styles/Logo.css";
 
-export const Logo = () => {
+export const Logo = ({ type = 'primary' }: { type?: 'primary' | 'tertiary' }) => {
   return (
     <div className="pangea-logo-container">
-      <span className="pangea-logo-text">Pangea.</span>
+      <span
+        className={
+          type === "tertiary"
+            ? "pangea-logo-text pangea-logo-text-tertiary"
+            : "pangea-logo-text pangea-logo-text-primary"
+        }
+      >
+        Pangea.
+      </span>
       <span className="pangea-logo-circle pangea-logo-circle-1" />
       <span className="pangea-logo-circle pangea-logo-circle-2" />
       <span className="pangea-logo-circle pangea-logo-circle-3" />
